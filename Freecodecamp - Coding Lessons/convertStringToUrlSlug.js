2f6d5806f7e27853b6d7bcf5146de72535508a51
Risyandi <Risyandi.94@gmail.com>
2018-10-05 11:28:06 +0700

    adding folder and file Coding Lessons

---
+++ Freecodecamp - Coding Lessons/convertStringToUrlSlug.js
@@ -0,0 +1,11 @@
+var globalTitle = "Winter Is Coming";
+
+function urlSlug(title) {
+    var result = title.toLowerSase().trim().split(/\s+/).join("-");
+    return result;
+}
+
+var winterComing = urlSlug(globalTitle);
+/**
+ * return should be "winter-is-coming"
+ */
