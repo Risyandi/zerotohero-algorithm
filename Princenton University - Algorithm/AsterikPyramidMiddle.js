6a5e243b90b43284288938dd5127e6a58f5c6459
Risyandi <Risyandi.94@gmail.com>
2018-11-15 15:25:01 +0700

    fixing asterik pyramid middle coloumn

--- Princenton University - Algorithm/AsterikPyramidMiddle.js
+++ Princenton University - Algorithm/AsterikPyramidMiddle.js
@@ -1,30 +1,56 @@
 function asterikPyramidMiddle(rows, symbol1, symbol2, symbol3) {
     for (let index = 0; index < rows; index++) {
-        console.log(index, "index");
-        // this is step one to print empty spaces
+        // console.log(index, "identity-row");
+
+        // (1) this is step one print symbol blank
         var space = "";
         for (let indexj = 0; indexj < (rows - index - 1); indexj++) {
-            console.log(indexj, "indexj");
-            space += symbol2;
+            // console.log(rows, index, "value-loops-step1");
+            // console.log(indexj, "print-col-step1");
+            space += symbol3;
         }
 
-        // this is step two to print asterik
+
+        // (2) this is step two print symbol asterik
         var asterik = "";
-        var middle = rows / 2; // flag middle
-        for (let indexk = 0; indexk < (2 * index); indexk++) {
-            console.log(indexk, "indexk", index, "index");
-            if (indexk > middle) {
-                asterik += symbol3;
-                console.log(indexk, index, "step1");
+        for (let indexk = 0; indexk < (index + 1); indexk++) {
+            // console.log(index, "value-loops-step2");
+            // console.log(indexk, "print-col-step2");
+            asterik += symbol1;
+        }
+
+
+        // (3) this is step three print symbol plus
+        var middleasterik = "";
+        var mid = (rows / 2) - 2;
+        for (let indexl = 0; indexl < index; indexl++) {
+            // console.log(index, "value-loops-step3");
+            // console.log(indexl, "print-col-step3");
+
+            // condition where are midle of coloumn  
+            if (indexl > mid) {
+                // console.log(indexl, mid);
+                middleasterik += symbol2;
             } else {
-                asterik += symbol1;
-                console.log(indexk, index, "step2");
+                middleasterik += symbol1;
             }
         }
-        // print asterik to console
-        console.log(space + asterik);
+
+        // print several step
+        console.log(space + asterik + middleasterik);
     }
 }
 
 // call function
-asterikPyramidMiddle(10, "*", " ", "-"); 
+asterikPyramidMiddle(5, "*", "+", " ");
+
+
+/**
+ * looping testing : 
+    var asterik2 = "";
+    var mid = rows / 2;
+    for (let indexl = (index + 1); indexl > mid; indexl--) {
+        console.log(indexl, "val-loops-step3");
+        asterik2 += symbol1;
+    }
+ */
