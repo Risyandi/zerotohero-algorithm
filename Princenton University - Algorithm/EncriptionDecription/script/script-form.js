95ee02437c24a43fae5b9f18bcacae3ebdbe02e8
Risyandi <Risyandi.94@gmail.com>
2018-12-28 14:29:54 +0700

    adding function encryption

--- Princenton University - Algorithm/EncriptionDecription/script/script-form.js
+++ Princenton University - Algorithm/EncriptionDecription/script/script-form.js
@@ -1,3 +1,39 @@
-function name(params) {
+var letter = 'abcdefghijklmnopqrstupwxyz';
+
+// function encryption
+function encryptionProcess() {
+    // get value from form encryption
+    var dataTextEnc = document.querySelector('#words-text-enc').value;
+        console.log(dataTextEnc, ":dataText");
+    var dataPassEnc = document.querySelector('#password-enc').value;
+        console.log(dataPassEnc, ":dataPass");
     
+    // text to lowercase
+    var lowerWords = dataTextEnc.toLowerCase();
+        console.log(lowerWords, ":lowerWords");
+    
+    //  looping data 
+    var result = "";
+    for (let index = 0; index < lowerWords.length; index++) {
+        var wordsData = lowerWords[index];
+            console.log(wordsData, ":wordsData");
+        var indexWords = letter.indexOf(wordsData);
+            console.log(indexWords, ":indexWords");
+
+        if (indexWords == -1) {
+            // condition meet space changes as number "32"
+            result += 32 + "-";
+        } else {
+            // condition once alphabet attach number "0"
+            result += ((indexWords + 1) + "0").toString();
+        }
+    }
+    // set value on input fields
+    document.querySelector('#result-enc').value = result;
 }
+    
+var elmEnc = document.getElementById("button-encrypt");
+elmEnc.addEventListener("click", encryptionProcess, false);
+
+
+// get value from form decryption
