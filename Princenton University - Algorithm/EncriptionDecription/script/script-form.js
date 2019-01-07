af0316f4c91c6909f24d1158e0e37edf39bbfa79
Risyandi <Risyandi.94@gmail.com>
2019-01-07 16:31:04 +0700

    modified script

--- Princenton University - Algorithm/EncriptionDecription/script/script-form.js
+++ Princenton University - Algorithm/EncriptionDecription/script/script-form.js
@@ -4,22 +4,22 @@ var letter = 'abcdefghijklmnopqrstuvwxyz';
 function encryptionProcess(dataTextEnc, dataPassEnc) {
     // get value from form encryption
     dataTextEnc = document.querySelector('#words-text-enc').value;
-        console.log(dataTextEnc, ":data text");
+    console.log(dataTextEnc, ":data text");
     dataPassEnc = document.querySelector('#password-enc').value;
-        console.log(dataPassEnc, ":data pass");
+    console.log(dataPassEnc, ":data pass");
 
     // text to lowercase
     var lowerWords = dataTextEnc.toLowerCase();
-        console.log(lowerWords, ":lower words");
-    
+    console.log(lowerWords, ":lower words");
+
     //  looping data for appear data
     var result = "";
     for (let index = 0; index < lowerWords.length; index++) {
         // init data
         var wordsData = lowerWords[index];
-            console.log(wordsData, ":words data");
+        console.log(wordsData, ":words data");
         var indexWords = letter.indexOf(wordsData);
-            console.log(indexWords, ":index words");
+        console.log(indexWords, ":index words");
         // check condition value of indexWords
         if (indexWords == -1) {
             // condition meet space changes as number "32"
@@ -37,47 +37,66 @@ function encryptionProcess(dataTextEnc, dataPassEnc) {
 function decryptionProcess() {
     // get value from form encryption and decryption
     dataPassEnc = document.querySelector('#password-enc').value;
-        console.log(dataPassEnc, ":dataPassEnc");
+    console.log(dataPassEnc, ":dataPassEnc");
     dataPassDec = document.querySelector('#password-dec').value;
-        console.log(dataPassDec, ":dataPassDec");
+    console.log(dataPassDec, ":dataPassDec");
     dataTextEnc = document.querySelector('#words-text-dec').value;
-        console.log(dataTextEnc, ":data encryption");
-    
+    console.log(dataTextEnc, ":data encryption");
+
     // check value of password
     if (dataPassEnc != dataPassDec) {
         alert("password tidak sama, harap masukan password yang sama !");
         // call action if you have wrong password
-    } else 
-    if (dataPassEnc == dataPassDec) {
-        // test alert
-        // alert("password sama!");
-        
-        // remove 32-
-        var replaceOne = dataTextEnc.replace(/(\d\d-)/g, " ");
+    } else
+        if (dataPassEnc == dataPassDec) {
+            // test alert
+            // alert("password sama!");
+
+            // remove 32-
+            var replaceOne = dataTextEnc.replace(/(\d\d-)/g, " ");
             console.log(replaceOne, ":one");
-        // remove duplicate zero number
-        var replaceTwo = replaceOne.replace(/(00)/g, "v-");
+            // remove duplicate zero number
+            var replaceTwo = replaceOne.replace(/(00)/g, "v-");
             console.log(replaceTwo, ":two");
-        // remove zero number as separate
-        var replaceThree = replaceTwo.replace(/(0)/g, "-");
+            // remove zero number as separate
+            var replaceThree = replaceTwo.replace(/(0)/g, "-");
             console.log(replaceThree, ":three");
-        // remove symbol v as zero number
-        var resultDecrFinal = replaceThree.replace(/(v)/g, "0");
+            // remove symbol v as zero number
+            var resultDecrFinal = replaceThree.replace(/(v)/g, "0");
             console.log(resultDecrFinal, ":final");
 
-        // convert number to letter
-        var finishResult = resultDecrFinal.replace(/([\d]{1,2})([^\d]|$)/g, function (match, p1, p2) {
-            return letter[Number(p1) - 1] + (p2 == "-" ? "" : p2);
-        });
+            // convert number to letter
+            var finishResult = resultDecrFinal.replace(/([\d]{1,2})([^\d]|$)/g, function (match, p1, p2) {
+                return letter[Number(p1) - 1] + (p2 == "-" ? "" : p2);
+            });
 
-        document.querySelector('#result-dec').value = finishResult;
+            document.querySelector('#result-dec').value = finishResult;
+        }
+}
+
+
+function emptyFieldEncrypt() {
+    var a = document.querySelector('#words-text-enc').value;
+    var b = document.querySelector('#password-enc').value;
+    if (a==null || a=="", b==null || b=="") {
+        alert('Please field your form encryption');
+    }
+}
+
+function emptyFieldDecrypt() {
+    var c = document.querySelector('#words-text-dec').value;
+    var d = document.querySelector('#password-dec').value;
+    if (c==null || c=="", d==null || d=="") {
+        alert('Please field your form decryption');
     }
 }
 
 // listen event click for process encryption
 var elmEnc = document.getElementById("button-encrypt");
     elmEnc.addEventListener("click", encryptionProcess, false);
+    elmEnc.addEventListener("click", emptyFieldEncrypt, false);
 
 // listen event click for process decryption
 var elmDec = document.getElementById("button-decrypt");
     elmDec.addEventListener("click", decryptionProcess, false);
+    elmDec.addEventListener("click", emptyFieldDecrypt, false);
